e6bac5101c92d4871a1bc66f072d3bf79cb8a36d
Fadi NOUFAL <noufal4me@gmail.com>
2019-09-01 17:36:06 +0200

    Create reducers

--- src/js/reducers.js
+++ src/js/reducers.js
@@ -1,12 +1,14 @@
 import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
 import appReducer from "./reducers/app";
+import authReducer from "./reducers/auth";
 import postsReducer from "./reducers/feed";
 
 const createRootReducer = history =>
     combineReducers({
         router: connectRouter(history),
         app: appReducer,
+        auth: authReducer,
         feed: postsReducer
     });
 
