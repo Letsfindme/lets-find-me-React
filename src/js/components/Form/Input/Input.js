c376b77759a1bf56409d40aff61014fc8d4eddf9
Fadi NOUFAL <noufal4me@gmail.com>
2019-09-01 17:33:55 +0200

    Input materialize style

--- src/js/components/Form/Input/Input.js
+++ src/js/components/Form/Input/Input.js
@@ -1,40 +1,80 @@
-import React from 'react';
+import React from "react";
 
-import './Input.css';
+import "./Input.less";
 
 const input = props => (
-  <div className="input">
-    {props.label && <label htmlFor={props.id}>{props.label}</label>}
-    {props.control === 'input' && (
+  <div className="wrapper">
+    <div className="group">
+      {props.control === "input" && (
         <input
-        className={[
-          !props.valid ? 'invalid' : 'valid',
-          props.touched ? 'touched' : 'untouched'
-        ].join(' ')}
-        type={props.type}
-        id={props.id}
-        required={props.required}
-        value={props.value}
-        placeholder={props.placeholder}
-        onChange={e => props.onChange(props.id, e.target.value, e.target.files)}
-        onBlur={props.onBlur}
-      />
-    )}
-    {props.control === 'textarea' && (
-      <textarea
-        className={[
-          !props.valid ? 'invalid' : 'valid',
-          props.touched ? 'touched' : 'untouched'
-        ].join(' ')}
-        id={props.id}
-        rows={props.rows}
-        required={props.required}
-        value={props.value}
-        onChange={e => props.onChange(props.id, e.target.value)}
-        onBlur={props.onBlur}
-      />
-    )}
+          type="text"
+          required="required"
+          className={[
+            !props.valid ? "invalid" : "valid",
+            props.touched ? "touched" : "untouched"
+          ].join(" ")}
+          type={props.type}
+          id={props.id}
+          value={props.value}
+          onChange={e =>
+            props.onChange(props.id, e.target.value, e.target.files)
+          }
+          onBlur={props.onBlur}
+        />
+      )}
+      {props.control === "textarea" && (
+        <textarea
+          className={[
+            !props.valid ? "invalid" : "valid",
+            props.touched ? "touched" : "untouched"
+          ].join(" ")}
+          id={props.id}
+          rows={props.rows}
+          required={props.required}
+          value={props.value}
+          onChange={e => props.onChange(props.id, e.target.value)}
+          onBlur={props.onBlur}
+        />
+      )}
+      <span className="highlight" />
+      <span className="bar" />
+      {props.label && <label htmlFor={props.id}>{props.label}</label>}
+    </div>
   </div>
+  // <div>
+  //   {props.label && <label htmlFor={props.id}>{props.label}</label>}
+  //   {props.control === 'input' && (
+  //       <input
+  //       className={[
+  //         'common-input',
+  //         !props.valid ? 'invalid' : 'valid',
+  //         props.touched ? 'touched' : 'untouched'
+  //       ].join(' ')}
+  //       type={props.type}
+  //       id={props.id}
+  //       required={props.required}
+  //       value={props.value}
+  //       placeholder={props.placeholder}
+  //       onChange={e => props.onChange(props.id, e.target.value, e.target.files)}
+  //       onBlur={props.onBlur}
+  //       placeholder={props.placeholder}
+  //     />
+  //   )}
+  //   {props.control === 'textarea' && (
+  //     <textarea
+  //       className={[
+  //         !props.valid ? 'invalid' : 'valid',
+  //         props.touched ? 'touched' : 'untouched'
+  //       ].join(' ')}
+  //       id={props.id}
+  //       rows={props.rows}
+  //       required={props.required}
+  //       value={props.value}
+  //       onChange={e => props.onChange(props.id, e.target.value)}
+  //       onBlur={props.onBlur}
+  //     />
+  //   )}
+  // </div>
 );
 
 export default input;
