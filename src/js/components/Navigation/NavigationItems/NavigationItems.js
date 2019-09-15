42b127b40ed4ff32bb60c4d67702c94b05c6dac7
Fadi NOUFAL <noufal4me@gmail.com>
2019-09-15 16:31:37 +0200

    Change navigation items

--- src/js/components/Navigation/NavigationItems/NavigationItems.js
+++ src/js/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,16 +1,15 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "./NavigationItems.less";
-
-
-
 const navigationItems = props => [
   ...props.navItems
     .filter(item => item.onAuth === props.isAuth || item.show === true)
     .map(item => (
-      <li key={item.id} className={["navigation-item", "white", item.className].join(" ")}>
-        <NavLink to={item.link} exact onClick={() =>props.onChoose(item)}>
-          {item.text}
+      <li
+        key={item.id}
+        className={["navigation-item", item.className].join(" ")}
+      >
+        <NavLink to={item.link} exact onClick={() => props.onChoose(item)}>
+          {item.logo} <span className="hidemeonxs">{item.text}</span>
         </NavLink>
       </li>
     ))
