efa8c3ea3f453750e1430db77e07b3a25cfef2e6
Fadi NOUFAL <noufal4me@gmail.com>
2019-09-15 16:28:08 +0200

    Variable image loading

--- src/js/components/Image/Image.jsx
+++ src/js/components/Image/Image.jsx
@@ -1,14 +1,30 @@
 import React from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Image.css";
 
-export default props => (
-  <div
-    className= {props.className ? props.className : "image"}
-    style={{
-      backgroundImage: `url('${"http://localhost:8080/" + props.imageUrl}')`,
-      backgroundSize: props.contain ? "contain" : "cover",
-      backgroundPosition: props.backgroundPosition ? props.backgroundPosition : "center"
-    }}
-  />
-);
+export default props =>
+  props.previews ? (
+    <div
+      className={props.className}
+      style={{
+        backgroundImage: `url('${props.imageUrl}')`,
+        backgroundSize: props.contain ? "contain" : "cover",
+        backgroundPosition: props.backgroundPosition
+          ? props.backgroundPosition
+          : "center"
+      }}
+    >
+      <span onClick={props.onClick}><FontAwesomeIcon icon="trash-alt"/></span>
+    </div>
+  ) : (
+    <div
+      className={props.className ? props.className : "image"}
+      style={{
+        backgroundImage: `url('${"http://localhost:8080/" + props.imageUrl}')`,
+        backgroundSize: props.contain ? "contain" : "cover",
+        backgroundPosition: props.backgroundPosition
+          ? props.backgroundPosition
+          : "center"
+      }}
+    />
+  );
