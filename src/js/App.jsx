c1ce33592546bdf8dbb70147db22750906a452eb
Fadi NOUFAL <noufal4me@gmail.com>
2019-09-15 16:24:55 +0200

    Manage user auth

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -7,16 +7,97 @@ import MobileNavigation from "./components/Navigation/MobileNavigation/MobileNav
 import { useDispatch, useSelector } from "react-redux";
 import Routes from "./pages/Router/routes";
 import "./App.less";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas} from '@fortawesome/free-solid-svg-icons'
+library.add(fab, fas);
 
 export default props => {
+  const getAuth = useSelector(state => state.auth.isAuthenticated);
   const [showBackdrop, setShowBackdrop] = useState(false);
   const [showMobileNav, setShowMobileNav] = useState(false);
   const [error, setError] = useState(null);
   const dispatch = useDispatch();
-  const getAuth = useSelector(state => state.auth.isAuthenticated);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const expiryDate = localStorage.getItem("expiryDate");
+    if (!token || !expiryDate) {
+      return;
+    }
+    if (new Date(expiryDate) <= new Date()) {
+      logoutHandler();
+      return;
+    }
+    if (!getAuth) {
+      const userId = localStorage.getItem("userId");
+      const remainingMilliseconds =
+        new Date(expiryDate).getTime() - new Date().getTime();
+      dispatch({ type: "SET_TOK", payload: token });
+      dispatch({ type: "SET_UID", payload: userId });
+      setAutoLogout(remainingMilliseconds);
+      dispatch({ type: "SET_ISAUTH", payload: true });
+    }
+  }, []);
+  const signupHandler = (event, authData) => {
+    event.preventDefault();
+    setAuthLoading(true);
+    fetch("http://localhost:8080/auth/signup", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email: authData.signupForm.email.value,
+        password: authData.signupForm.password.value,
+        name: authData.signupForm.name.value
+      })
+    })
+      .then(res => {
+        if (res.status === 422) {
+          throw new Error(
+            "Validation failed. Make sure the email address isn't used yet!"
+          );
+        }
+        if (res.status !== 200 && res.status !== 201) {
+          console.log("Error!");
+          throw new Error("Creating a user failed!");
+        }
+        return res.json();
+      })
+      .then(resData => {
+        dispatch({ type: "SET_ISAUTH", payload: false });
+        setAuthLoading(false);
+        this.props.history.replace("/");
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch({ type: "SET_AUTH_LOADING", payload: false });
+        setAuthLoading(false);
+        setError(err);
+      });
+  };
 
+  const setAutoLogout = milliseconds => {
+    setTimeout(() => {
+      logoutHandler();
+    }, milliseconds);
+  };
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  const logoutHandler = () => {
+    dispatch({ type: "SET_ISAUTH", payload: false });
+    dispatch({ type: "SET_TOK", payload: null });
+    localStorage.removeItem("token");
+    localStorage.removeItem("expiryDate");
+    localStorage.removeItem("userId");
+
+    props.history.push("/");
+  };
+  
   const mobileNavHandler = isOpen => {
     setShowMobileNav(isOpen);
     setShowBackdrop(isOpen);
@@ -28,6 +109,8 @@ export default props => {
     setError(null);
   };
 
+ 
+
   return (
     <Fragment>
       {showBackdrop && <Backdrop onClick={backdropClickHandler} />}
@@ -36,7 +119,7 @@ export default props => {
         <Layout
           header={
             <MainNavigation
-              {...props}
+             {...props}
               onOpenMobileNav={mobileNavHandler.bind(this, true)}
               isAuth={getAuth}
             />
