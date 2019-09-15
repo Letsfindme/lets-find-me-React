928fe102385d24a8c80708fdb29d5b20c8246351
Fadi NOUFAL <noufal4me@gmail.com>
2019-09-15 16:31:08 +0200

    Add subnavigation

---
+++ src/js/components/Navigation/MainNavigation/SubMenu.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default Submenu => {
+  <ul className="nav__submenu" key="transition-group-content" >
+    <li className="nav__submenu-item ">
+      <a>Our Company</a>
+    </li>
+    <li className="nav__submenu-item ">
+      <a>Our Team</a>
+    </li>
+    <li className="nav__submenu-item ">
+      <a>Our Portfolio</a>
+    </li>
+  </ul>;
+};
