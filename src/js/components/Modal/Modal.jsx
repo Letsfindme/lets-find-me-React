053ad36bb6c5bb545d900e8fe2c32073fc748d64
Fadi NOUFAL <noufal4me@gmail.com>
2019-10-01 10:12:39 +0200

    Fix modal button

--- src/js/components/Modal/Modal.jsx
+++ src/js/components/Modal/Modal.jsx
@@ -1,31 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import Button from '../Button/Button.jsx';
-
+import Button from "../Button/Button.jsx";
 
 const modal = props =>
   ReactDOM.createPortal(
-    <div className="modal">
-      <header className="modal__header">
-        <h1>{props.title} , {props.acceptEnabled}</h1>
-      </header>
+    <div className={["modal", props.mobile ? "modal-mobile" : ""].join(' ')}>
+      {!props.noHeader ? (
+        <header className="modal__header">
+          <h1>
+            {props.title} {props.acceptEnabled}
+          </h1>
+        </header>
+      ) : (
+        ""
+      )}
+
       <div className="modal__content">{props.children}</div>
-      <div className="modal__actions">
-        <Button design="danger" mode="flat" onClick={props.onCancelModal}>
-          Cancel
-        </Button>
-        <Button
-          mode="raised"
-          onClick={props.onAcceptModal}
-          disabled={!props.acceptEnabled}
-          loading={props.isLoading}
-        >
-          Ouiiii
-        </Button>
-      </div>
+      {!props.noAction ? (
+        <div className="modal__actions">
+          <Button design="danger" mode="flat" onClick={props.onCancelModal}>
+            Cancel
+          </Button>
+          <Button
+            mode="raised"
+            onClick={props.onAcceptModal}
+            disabled={!props.acceptEnabled}
+            loading={props.isLoading}
+          >
+            Ouiiii
+          </Button>
+        </div>
+      ) : (
+        ""
+      )}
     </div>,
-    document.getElementById('modal-root')
+    document.getElementById("modal-root")
   );
 
 export default modal;
