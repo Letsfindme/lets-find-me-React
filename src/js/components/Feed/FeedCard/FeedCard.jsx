f07a3e0125105051356615d55bb2606bba55475b
Fadi NOUFAL <noufal4me@gmail.com>
2019-10-03 18:23:00 +0200

    Feedcard user name

--- src/js/components/Feed/FeedCard/FeedCard.jsx
+++ src/js/components/Feed/FeedCard/FeedCard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "../../Image/Image";
 import { Link } from "react-router-dom";
+import Moment from "react-moment";
 
 export default props => (
   <div className="post-card" link={"/feed/" + props.id}>
@@ -10,7 +11,23 @@ export default props => (
         <span>{props.title}</span>
         <p>{props.content}</p>
       </article>
-      <div className="post-header">
+
+      <li className="post-content comment-contain">
+        <Image
+          key={Date.now()}
+          className={"user-profile"}
+          imageUrl={props.image}
+          left
+        />
+        <div>
+          <div className="comment-right">
+            <h5>{props.author}</h5>
+            <Moment format="YYYY/MM/DD hh:mm">{props.date}</Moment>
+          </div>
+        </div>
+      </li>
+
+      {/* <div className="post-header">
         <Image
           imageUrl={props.image}
           contain
@@ -22,7 +39,7 @@ export default props => (
           <div className="date">{props.date}</div>
         </div>
         <div className="spacer" />
-      </div>
+      </div> */}
     </Link>
   </div>
 );
