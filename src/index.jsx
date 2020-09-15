93b42717657e54b3c1f2e6db5741505889b7d10b
Fadi NOUFAL <noufal4me@gmail.com>
2020-09-15 13:13:27 +0200

    Fix import App

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { Router } from "react-router-dom";
 import configureStore, { history } from "./js/store";
-import App from "./js/app";
+import App from "./js/App";
 const MOUNT_NODE = document.getElementById("app");
 const store = configureStore();
 
